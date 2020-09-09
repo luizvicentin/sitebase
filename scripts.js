d77850d7ed73c1705e021abe08570946b9be4396
Luiz Fernando Vicentin Pavanello <luizpavanello@gmail>
2020-09-09 13:58:01 -0300

    Final Update

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
-//Function expression to select elements
+// Function expression to select elements.
 const selectElement = (s) => document.querySelector(s)
-// Open the menu on click
+
+//Open the menu on click
 selectElement('.open').addEventListener('click', () => {
-    selectElement('.nav-list').classList.add(active);
+  selectElement('.nav-list').classList.add('active');
 })
+
 //Close the menu on click
 selectElement('.close').addEventListener('click', () => {
-    selectElement('.nav-list').classList.remove(active);
+  selectElement('.nav-list').classList.remove('active');
 })
+
